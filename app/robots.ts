540bb09935ad86fc25cb60d178e7ba8739954608
tebitan26 <luna.esteban26@gmail.com>
2025-09-13 20:00:02 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,13 @@
-// app/robots.ts
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   const host = "https://www.sidetick.app";
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/", "/admin"],
+    },
     sitemap: `${host}/sitemap.xml`,
     host,
   };
