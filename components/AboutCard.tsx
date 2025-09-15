11b17b4e3a3b4a957a6709839d6ffb67e50228bc
tebitan26 <luna.esteban26@gmail.com>
2025-09-15 14:13:20 +0300

    Update AboutCard.tsx

--- components/AboutCard.tsx
+++ components/AboutCard.tsx
@@ -3,7 +3,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/cn";
 
 type LinkDef = { href: string; label: string; icon?: "linkedin" | "web" };
 type Props = {
