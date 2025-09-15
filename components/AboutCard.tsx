98f72701518b4e0edad26350cf77bc5d42b74275
tebitan26 <luna.esteban26@gmail.com>
2025-09-15 15:32:16 +0300

    Update AboutCard.tsx

--- components/AboutCard.tsx
+++ components/AboutCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/cn";
 
 type LinkItem = { href: string; label: string; icon?: "linkedin" | "web" };
 
