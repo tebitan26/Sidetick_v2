57d59481f7711fe2e6c0d24b7198426f95be6439
tebitan26 <luna.esteban26@gmail.com>
2025-09-17 08:35:01 +0300

    Create SmartImage.tsx

---
+++ components/ui/SmartImage.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image, { ImageProps } from "next/image";
+import { useState } from "react";
+import { Skeleton } from "./Skeleton";
+import { cn } from "@/lib/utils";
+
+export function SmartImage({ className, ...props }: ImageProps & { className?: string }) {
+  const [loaded, setLoaded] = useState(false);
+  return (
+    <div className={cn("relative", className)}>
+      {!loaded && <Skeleton className="absolute inset-0" rounded="rounded-2xl" />}
+      <Image
+        {...props}
+        onLoad={() => setLoaded(true)}
+        className={cn("transition-opacity duration-500", loaded ? "opacity-100" : "opacity-0")}
+      />
+    </div>
+  );
+}
