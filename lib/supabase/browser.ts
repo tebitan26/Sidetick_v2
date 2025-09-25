572e11200e272411c1f2d86a23063d72a47f1f80
tebitan26 <luna.esteban26@gmail.com>
2025-09-25 18:26:15 +0300

    Update browser.ts

--- lib/supabase/browser.ts
+++ lib/supabase/browser.ts
@@ -1,10 +1,8 @@
-// lib/supabase/browser.ts
-"use client";
 import { createBrowserClient } from "@supabase/ssr";
 
-// ✅ Export nommé (correspond à ton import { supabaseBrowser } ...)
-export const supabaseBrowser = () =>
-  createBrowserClient(
+export function supabaseBrowser() {
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+}
