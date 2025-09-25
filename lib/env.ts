8a6adbbc356270fe1fe869ea211ac3b4820bb92e
tebitan26 <luna.esteban26@gmail.com>
2025-09-25 13:17:11 +0300

    Update env.ts

--- lib/env.ts
+++ lib/env.ts
@@ -1,2 +1,6 @@
+// lib/env.ts
 export const SHOW_WAITLIST_COUNT =
   process.env.NEXT_PUBLIC_SHOW_WAITLIST_COUNT === "true";
+
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL || "https://www.sidetick.app";
